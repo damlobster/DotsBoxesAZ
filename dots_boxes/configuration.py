022649d53c483e5425425f517f2ffec8f2e70444
Martin <damien.martin@epfl.ch>
2018-09-28 12:03:50 +0200

    remove old config files

--- dots_boxes/configuration.py
+++
@@ -1,8 +0,0 @@
-from dots_boxes.dots_boxes_game import BoxesState
-import sys
-sys.path.append('..')
-from project.default_config import DefaultConfig
-
-class DotsBoxesConfig(DefaultConfig):
-  GAME_NEW_STATE = lambda _: BoxesState((3,3))
-  MCTS_NUM_READ = 500 
