ab939cf7eb5ec09a4f3570f753a6c32f008f8c4e
Martin <damien.martin@epfl.ch>
2018-10-05 22:15:02 +0200

    use function create_root_uct_node()

--- mcts_tests.py
+++ mcts_tests.py
@@ -1,4 +1,4 @@
-from mcts import UCT_search
+from mcts import UCT_search, create_root_uct_node
 import unittest
 import numpy as np
 
@@ -9,9 +9,10 @@ class MCTSTestCase(unittest.TestCase):
         from dots_boxes.dots_boxes_game import BoxesState
         import time
         import resource
-        num_reads = 11
+        num_reads = 1000
+        root_node = create_root_uct_node(BoxesState())
         tick = time.time()
-        policy = UCT_search(BoxesState((1, 1)), num_reads,
+        policy = UCT_search(root_node, num_reads,
                             lambda state: (np.ones(state.get_actions_size()), 0))
         print(policy.astype(int))
         tock = time.time()
