bfb74672753edd851edbe3accb41262eae0a8c6e
Martin <damien.martin@epfl.ch>
2018-10-27 19:48:25 +0200

    use cyclic learing rate, log train and val losses to different tensorboard log

--- nn.py
+++ nn.py
@@ -1,9 +1,11 @@
+import Optimizer
 import asyncio
 import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
 import torch.utils.data as data
+from tensorboardX import SummaryWriter
 import pandas as pd
 
 import utils
@@ -128,6 +130,9 @@ class NeuralNetWrapper():
         self.params = params
         self.device = torch.device(
             params.nn.pytorch_device if torch.cuda.is_available() else "cpu")
+        self.model = model.to(self.device) if model is not None else None
+
+    def set_model(self, model):
         self.model = model.to(self.device)
 
     def predict_sync(self, X):
@@ -150,27 +155,35 @@ class NeuralNetWrapper():
         res = await self.predict(X)
         return res
 
-    def train(self, train_dataset, val_dataset, writer, last_batch_idx):
+    def train(self, train_dataset, val_dataset, last_batch_idx):
         params = self.params.nn.train_params
         
+        train_log = SummaryWriter(params.tensorboard_log+"/train")
+        eval_log = SummaryWriter(params.tensorboard_log+"/eval")
+
         train_data = data.DataLoader(
             train_dataset, params.train_batch_size, shuffle=True)
         validation_data = data.DataLoader(
             val_dataset, params.val_batch_size) if val_dataset is not None else None
 
-        optimizer = torch.optim.Adam(
-            self.model.parameters(), lr=params.lr, **params.adam_params)
-        #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, *params.lr_scheduler)
-
         criterion = AlphaZeroLoss()
+
         batch_i = last_batch_idx + 1
+        lr = params.lr
+        optimizer = torch.optim.Adam(
+            self.model.parameters(), lr=lr, **params.adam_params)
+        lr_scheduler = CyclicLR(optimizer,
+                                base_lr=lr,
+                                max_lr=lr*params.lr_scheduler.max_lr_factor,
+                                step_size=params.lr_scheduler.step_size,
+                                last_batch_iteration=batch_i)
+
         for epoch in range(params.nb_epochs):
-            #lr_scheduler.step() #TODO use batch number
 
             self.model.train(True)
-            n_batches = len(train_data)
             tr_loss = 0
             for boards, pi, z in train_data:
+                scheduler.batch_step()
                 batch_i += 1
                 # Transfer to GPU
                 boards = boards.to(self.device)
@@ -179,18 +192,18 @@ class NeuralNetWrapper():
 
                 p, v = self.model(boards)
                 loss, (loss_pi, loss_v) = criterion(p, v, pi, z)
-                loss = loss/n_batches
+                #loss = loss/n_batches
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
 
-                loss_v, loss_pi = loss_v/n_batches, loss_pi/n_batches
+                loss_v, loss_pi = loss_v, loss_pi
                 tr_loss += loss_pi + loss_v
-                # write scalar to tensorboard
-                writer.add_scalar('data/loss/train/pi', loss_pi, batch_i) #, walltime=batch_i)
-                writer.add_scalar('data/loss/train/v', loss_v, batch_i)
-                writer.add_scalar('data/loss/train/total', loss_pi + loss_v, batch_i)
-                writer.add_scalar("data/lr", params.lr, batch_i)
+                # write scalars to tensorboard
+                train_log.add_scalar('loss/pi', loss_pi, batch_i) #, walltime=batch_i)
+                train_log.add_scalar('loss/v', loss_v, batch_i)
+                train_log.add_scalar('loss/total', loss_pi + loss_v, batch_i)
+                train_log.add_scalar("lr", params.lr, batch_i)
             
             val_loss = 0.0
             if val_dataset:
@@ -203,17 +216,27 @@ class NeuralNetWrapper():
                     z = z.to(self.device)
 
                     p, v = self.model(boards)
-                    loss, (loss_pi, loss_v) = criterion(p, v, pi, z)
-                    loss_v, loss_pi = loss_v/n_batches, loss_pi/n_batches
-                loss = loss.item()
-                val_loss += loss
-                writer.add_scalar('data/loss/val/pi', loss_pi, batch_i)
-                writer.add_scalar('data/loss/val/v', loss_v, batch_i)
-                writer.add_scalar('data/loss/val/total', loss, batch_i)
+                    _, (loss_pi, loss_v) = criterion(p, v, pi, z)
+                    loss_v += loss_v
+                    loss_pi += loss_pi
+
+                # get eval loss average
+                n_batches = len(validation_data)
+                loss_v /= n_batches
+                loss_pi /= n_batches
+                val_loss = loss_v + loss_pi
+                eval_log.add_scalar('loss/pi', loss_pi, batch_i)
+                eval_log.add_scalar('loss/v', loss_v, batch_i)
+                eval_log.add_scalar('loss/total', val_loss, batch_i)
 
             print("Epoch {}, train loss= {:5f}, validation loss= {:5f}".format(
-                epoch, tr_loss, val_loss,), flush=True)
-        
+                epoch, tr_loss/len(train_data), val_loss), flush=True)
+
+        train_log.export_scalars_to_json(params.tensorboard_log+"train_{}.json".format(last_batch_idx))
+        train_log.close()
+        train_log.export_scalars_to_json(params.tensorboard_log+"eval_{}.json".format(last_batch_idx))
+        eval_log.close()
+
         return batch_i
 
     def save_model_parameters(self, generation=None):
@@ -224,3 +247,152 @@ class NeuralNetWrapper():
 
     def load_model_parameters(self, generation, to_device=None):
         self.model.load_parameters(generation, to_device=to_device)
+
+
+class CyclicLR(object):
+    """Sets the learning rate of each parameter group according to
+    cyclical learning rate policy (CLR). The policy cycles the learning
+    rate between two boundaries with a constant frequency, as detailed in
+    the paper `Cyclical Learning Rates for Training Neural Networks`_.
+    The distance between the two boundaries can be scaled on a per-iteration
+    or per-cycle basis.
+    Cyclical learning rate policy changes the learning rate after every batch.
+    `batch_step` should be called after a batch has been used for training.
+    To resume training, save `last_batch_iteration` and use it to instantiate `CycleLR`.
+    This class has three built-in policies, as put forth in the paper:
+    "triangular":
+        A basic triangular cycle w/ no amplitude scaling.
+    "triangular2":
+        A basic triangular cycle that scales initial amplitude by half each cycle.
+    "exp_range":
+        A cycle that scales initial amplitude by gamma**(cycle iterations) at each
+        cycle iteration.
+    This implementation was adapted from the github repo: `bckenstler/CLR`_
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        base_lr (float or list): Initial learning rate which is the
+            lower boundary in the cycle for eachparam groups.
+            Default: 0.001
+        max_lr (float or list): Upper boundaries in the cycle for
+            each parameter group. Functionally,
+            it defines the cycle amplitude (max_lr - base_lr).
+            The lr at any cycle is the sum of base_lr
+            and some scaling of the amplitude; therefore
+            max_lr may not actually be reached depending on
+            scaling function. Default: 0.006
+        step_size (int): Number of training iterations per
+            half cycle. Authors suggest setting step_size
+            2-8 x training iterations in epoch. Default: 2000
+        mode (str): One of {triangular, triangular2, exp_range}.
+            Values correspond to policies detailed above.
+            If scale_fn is not None, this argument is ignored.
+            Default: 'triangular'
+        gamma (float): Constant in 'exp_range' scaling function:
+            gamma**(cycle iterations)
+            Default: 1.0
+        scale_fn (function): Custom scaling policy defined by a single
+            argument lambda function, where
+            0 <= scale_fn(x) <= 1 for all x >= 0.
+            mode paramater is ignored
+            Default: None
+        scale_mode (str): {'cycle', 'iterations'}.
+            Defines whether scale_fn is evaluated on
+            cycle number or cycle iterations (training
+            iterations since start of cycle).
+            Default: 'cycle'
+        last_batch_iteration (int): The index of the last batch. Default: -1
+    Example:
+        >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+        >>> scheduler = torch.optim.CyclicLR(optimizer)
+        >>> data_loader = torch.utils.data.DataLoader(...)
+        >>> for epoch in range(10):
+        >>>     for batch in data_loader:
+        >>>         scheduler.batch_step()
+        >>>         train_batch(...)
+    .. _Cyclical Learning Rates for Training Neural Networks: https://arxiv.org/abs/1506.01186
+    .. _bckenstler/CLR: https://github.com/bckenstler/CLR
+    """
+    def __init__(self, optimizer, base_lr=1e-3, max_lr=6e-3,
+                 step_size=2000, mode='triangular', gamma=1.,
+                 scale_fn=None, scale_mode='cycle', last_batch_iteration=-1):
+
+        if not isinstance(optimizer, Optimizer):
+            raise TypeError('{} is not an Optimizer'.format(
+                type(optimizer).__name__))
+        self.optimizer = optimizer
+
+        if isinstance(base_lr, list) or isinstance(base_lr, tuple):
+            if len(base_lr) != len(optimizer.param_groups):
+                raise ValueError("expected {} base_lr, got {}".format(
+                    len(optimizer.param_groups), len(base_lr)))
+            self.base_lrs = list(base_lr)
+        else:
+            self.base_lrs = [base_lr] * len(optimizer.param_groups)
+
+        if isinstance(max_lr, list) or isinstance(max_lr, tuple):
+            if len(max_lr) != len(optimizer.param_groups):
+                raise ValueError("expected {} max_lr, got {}".format(
+                    len(optimizer.param_groups), len(max_lr)))
+            self.max_lrs = list(max_lr)
+        else:
+            self.max_lrs = [max_lr] * len(optimizer.param_groups)
+
+        self.step_size = step_size
+
+        if mode not in ['triangular', 'triangular2', 'exp_range'] \
+                and scale_fn is None:
+            raise ValueError('mode is invalid and scale_fn is None')
+
+        self.mode = mode
+        self.gamma = gamma
+
+        if scale_fn is None:
+            if self.mode == 'triangular':
+                self.scale_fn = self._triangular_scale_fn
+                self.scale_mode = 'cycle'
+            elif self.mode == 'triangular2':
+                self.scale_fn = self._triangular2_scale_fn
+                self.scale_mode = 'cycle'
+            elif self.mode == 'exp_range':
+                self.scale_fn = self._exp_range_scale_fn
+                self.scale_mode = 'iterations'
+        else:
+            self.scale_fn = scale_fn
+            self.scale_mode = scale_mode
+
+        self.batch_step(last_batch_iteration + 1)
+        self.last_batch_iteration = last_batch_iteration
+
+    def batch_step(self, batch_iteration=None):
+        if batch_iteration is None:
+            batch_iteration = self.last_batch_iteration + 1
+        self.last_batch_iteration = batch_iteration
+        for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+            param_group['lr'] = lr
+
+    def _triangular_scale_fn(self, x):
+        return 1.
+
+    def _triangular2_scale_fn(self, x):
+        return 1 / (2. ** (x - 1))
+
+    def _exp_range_scale_fn(self, x):
+        return self.gamma**(x)
+
+    def get_lr(self):
+        step_size = float(self.step_size)
+        cycle = np.floor(1 + self.last_batch_iteration / (2 * step_size))
+        x = np.abs(self.last_batch_iteration / step_size - 2 * cycle + 1)
+
+        lrs = []
+        param_lrs = zip(self.optimizer.param_groups,
+                        self.base_lrs, self.max_lrs)
+        for param_group, base_lr, max_lr in param_lrs:
+            base_height = (max_lr - base_lr) * np.maximum(0, (1 - x))
+            if self.scale_mode == 'cycle':
+                lr = base_lr + base_height * self.scale_fn(cycle)
+            else:
+                lr = base_lr + base_height * \
+                    self.scale_fn(self.last_batch_iteration)
+            lrs.append(lr)
+        return lrs
