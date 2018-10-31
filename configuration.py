79c94ca3a0f4404ac690ee10e582d738ffb20643
Martin <damien.martin@epfl.ch>
2018-10-31 13:59:44 +0100

    change resnet config

--- configuration.py
+++ configuration.py
@@ -75,13 +75,13 @@ simple33 = DotDict({
     }
 })
 
-
 resnet20 = DotDict({
-    "data_root": "data/resnet20",
-    "hdf_file": "data/resnet20/sp_data.hdf",
-    "tensorboard_log": "data/tboard/resnet20",
+    "data_root": "data/resnet3110",
+    "hdf_file": "data/resnet3110/sp_data.hdf",
+    "tensorboard_log": "data/tboard/resnet3310",
     "game": {
-        "clazz": BoxesState
+        "clazz": BoxesState,
+        "init": partial(BoxesState.init_static_fields, ((3, 3),)),
     },
     "self_play": {
         "n_workers": 11,
@@ -94,46 +94,52 @@ resnet20 = DotDict({
         "pytorch_devices": ["cuda:1", "cuda:0"],  # get_cuda_devices_list(),
         "mcts": {
             "mcts_num_read": 800,
-            "mcts_cpuct": 1.0,
-            # from 8th move we greedly take move with most visit count
-            "temperature": {0: 1.0, 5: 1e-50},
+            "mcts_cpuct": 2.0,
+            # from 6th move we greedly take move with most visit count
+            "temperature": {0: 1.0, 6: 1e-50},
             "max_async_searches": 64,
         }
     },
+    "elo": {
+        "hdf_file": "data/resnet3310/elo_data.hdf",
+        "n_games": 10,
+        "n_workers": 10,
+        "games_per_workers": 1
+    },
     "nn": {
         "train_params": {
-            "nb_epochs": 50,
+            "nb_epochs": 9,
             "train_split": 0.9,
             "train_batch_size": 512,
             "val_batch_size": 1024,
-            "lr": 0.001,
-            #"lr_scheduler": (10, 0.1),
+            "lr": 1e-4,
+            "lr_scheduler": {"max_lr_factor": 10, "step_size": 2000},
             "adam_params": {
                 "betas": (0.9, 0.999),
                 "weight_decay": 1e-5,
             },
         },
-        "model_class": ResNetZero,
+        "model_class": SimpleNN,
         "pytorch_device": "cuda:1",
-        "chkpts_filename": "data/resnet20/model_gen{}.pt",
+        "chkpts_filename": "data/resnet3310/model_gen{}.pt",
         "model_parameters": {
             "resnet": {
                 "in_channels": 3,
-                "nb_channels": 128,
+                "nb_channels": 256,
                 "kernel_size": 3,
                 "nb_blocks": 20
             },
             "policy_head": {
-                "in_channels": 128,
-                "nb_actions": BoxesState.NB_ACTIONS,
+                "in_channels": 256,
+                "nb_actions": 32,
             },
             "value_head": {
-                "in_channels": 128,
-                "nb_actions": BoxesState.NB_ACTIONS,
+                "in_channels": 256,
+                "nb_actions": 32,
                 "n_hidden": 128
             },
         }
     }
 })
 
-params = simple33
+params = resnet20
