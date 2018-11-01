1fbe39af27174af666c6a37b037f426767e22459
damien <damlobster@gmail.com>
2018-11-01 09:56:47 +0100

    use convs in resnet heads

--- nn.py
+++ nn.py
@@ -4,7 +4,6 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 import torch.utils.data as data
-from tensorboardX import SummaryWriter
 import pandas as pd
 
 import utils
@@ -17,18 +16,12 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.conv0 = _create_conv_layer(in_channels, nb_channels, kernel_size)
         self.resblocks = nn.Sequential(
-            # A bit of fancy Python
             *(ResBlock(nb_channels, kernel_size) for _ in range(nb_blocks))
         )
-        #self.avg = nn.AvgPool2d(kernel_size=28)
-        #self.fc = nn.Linear(nb_channels, 10)
 
     def forward(self, x):
         x = F.relu(self.conv0(x))
         x = self.resblocks(x)
-        #x = F.relu(self.avg(x))
-        #x = x.view(x.size(0), -1)
-        #x = self.fc(x)
         return x
 
 
@@ -60,33 +53,31 @@ def _create_conv_layer(in_channels, out_channels, kernel_size):
 
 
 class PolicyHead(nn.Module):
-    def __init__(self, in_channels, nb_actions):
+    def __init__(self, in_channels, activation_map, inner_channels, nb_actions):
         super(PolicyHead, self).__init__()
-        self.conv0 = nn.Conv2d(in_channels, 2, kernel_size=1)
-        self.bn0 = nn.BatchNorm2d(2)
-        self.fc = nn.Linear(nb_actions, nb_actions)
+        self.conv0 = nn.Conv2d(in_channels, inner_channels, kernel_size=activation_map)
+        self.bn0 = nn.BatchNorm2d(inner_channels)
+        self.fc = nn.Linear(inner_channels, nb_actions)
 
     def forward(self, x):
         x = self.conv0(x)
-        x = F.relu(self.bn0(x))
-        x = x.view(x.size(0), -1)
+        x = F.relu(self.bn0(x)).view(x.size(0), -1)
         x = self.fc(x)
         p = F.log_softmax(x, dim=1)
         return p
 
 
 class ValueHead(nn.Module):
-    def __init__(self, in_channels, nb_actions, n_hidden):
+    def __init__(self, in_channels, activation_map, n_hidden0, n_hidden1):
         super(ValueHead, self).__init__()
-        self.conv0 = nn.Conv2d(in_channels, 1, kernel_size=1)
-        self.bn0 = nn.BatchNorm2d(1)
-        self.fc0 = nn.Linear(nb_actions//2, n_hidden)
-        self.fc1 = nn.Linear(n_hidden, 1)
+        self.conv0 = nn.Conv2d(in_channels, n_hidden0, kernel_size=activation_map)
+        self.bn0 = nn.BatchNorm2d(n_hidden0)
+        self.fc0 = nn.Linear(n_hidden0, n_hidden1)
+        self.fc1 = nn.Linear(n_hidden1, 1)
 
     def forward(self, x):
         x = self.conv0(x)
-        x = self.bn0(F.relu(x))
-        x = x.view(x.size(0), -1)
+        x = self.bn0(F.relu(x)).view(x.size(0), -1)
         x = F.relu(self.fc0(x))
         x = F.relu(self.fc1(x))
         v = torch.tanh(x)
@@ -96,10 +87,10 @@ class ValueHead(nn.Module):
 class ResNetZero(nn.Module):
     def __init__(self, params):
         super(ResNetZero, self).__init__()
-        self.params = params.nn
-        self.resnet = ResNet(**params.nn.resnet)
-        self.value_head = ValueHead(**params.nn.value_head)
-        self.policy_head = PolicyHead(**params.nn.policy_head)
+        self.params = params
+        self.resnet = ResNet(**params.nn.model_parameters.resnet)
+        self.value_head = ValueHead(**params.nn.model_parameters.value_head)
+        self.policy_head = PolicyHead(**params.nn.model_parameters.policy_head)
 
     def forward(self, x):
         x = self.resnet(x)
@@ -107,13 +98,17 @@ class ResNetZero(nn.Module):
         v = self.value_head(x)
         return (p, v)
 
-    def save_parameters(self, filename):
-        print("Model saved to:", filename)
-        torch.save(self.state_dict(), filename)
+    def save_parameters(self, generation):
+        filename = self.params.nn.chkpts_filename
+        fn = filename.format(generation)
+        print("Model saved to:", fn, flush=True)
+        torch.save(self.state_dict(), fn)
 
-    def load_parameters(self, filename):
-        print("Model loaded from:", filename)
-        self.load_state_dict(torch.load(filename, map_location=lambda storage, loc: storage))
+    def load_parameters(self, generation, to_device=None):
+        filename = self.params.nn.chkpts_filename
+        fn = filename.format(generation)
+        print("Model loaded from:", fn, flush=True)
+        self.load_state_dict(torch.load(fn, map_location=to_device))
 
 class AlphaZeroLoss(nn.Module):
     def __init__(self):
@@ -123,6 +118,11 @@ class AlphaZeroLoss(nn.Module):
         loss_v = (z - v).pow(2).mean()
         loss_pi = -(pi * p).sum(1).mean()
         return loss_v + loss_pi, (loss_pi.item(), loss_v.item()) #loss:variable, (loss_v:float, loss_pi:float)
+
+def _err_cb(fut):
+    if fut.exception():
+        print(fut.exception(), flush=True)
+        raise fut.exception()
 
 class NeuralNetWrapper():
     def __init__(self, model, params):
@@ -136,7 +136,6 @@ class NeuralNetWrapper():
 
     def predict_sync(self, X):
         self.model.train(False)
-
         x = torch.tensor(X, dtype=torch.float32, device=self.device)
         p, v = self.model.forward(x)
         p, v = torch.exp(p).cpu().detach().numpy(), v.cpu().detach().numpy()
@@ -145,6 +144,7 @@ class NeuralNetWrapper():
     async def predict(self, X):
         loop = asyncio.get_event_loop()
         future = loop.run_in_executor(None, self.predict_sync, X)
+        future.add_done_callback(_err_cb) # to re-raise if exception occured in executor
         return await future
 
     async def predict_from_game(self, game_state):
@@ -154,10 +154,9 @@ class NeuralNetWrapper():
         res = await self.predict(X)
         return res
 
-    def train(self, train_dataset, val_dataset, last_batch_idx):
+    def train(self, train_dataset, val_dataset, writer, last_batch_idx):
+        #self.model = nn.DataParallel(self.model, device_ids=[1, 0])
         params = self.params.nn.train_params
-        
-        writer = SummaryWriter(self.params.tensorboard_log)
 
         train_data = data.DataLoader(
             train_dataset, params.train_batch_size, shuffle=True)
@@ -227,7 +226,6 @@ class NeuralNetWrapper():
                 epoch, tr_loss/len(train_data), val_loss), flush=True)
 
         #writer.export_scalars_to_json(self.params.tensorboard_log+"log_{}.json".format(last_batch_idx))
-        writer.close()
 
         return batch_i
 
@@ -239,152 +237,3 @@ class NeuralNetWrapper():
 
     def load_model_parameters(self, generation, to_device=None):
         self.model.load_parameters(generation, to_device=to_device)
-
-
-class CyclicLR(object):
-    """Sets the learning rate of each parameter group according to
-    cyclical learning rate policy (CLR). The policy cycles the learning
-    rate between two boundaries with a constant frequency, as detailed in
-    the paper `Cyclical Learning Rates for Training Neural Networks`_.
-    The distance between the two boundaries can be scaled on a per-iteration
-    or per-cycle basis.
-    Cyclical learning rate policy changes the learning rate after every batch.
-    `batch_step` should be called after a batch has been used for training.
-    To resume training, save `last_batch_iteration` and use it to instantiate `CycleLR`.
-    This class has three built-in policies, as put forth in the paper:
-    "triangular":
-        A basic triangular cycle w/ no amplitude scaling.
-    "triangular2":
-        A basic triangular cycle that scales initial amplitude by half each cycle.
-    "exp_range":
-        A cycle that scales initial amplitude by gamma**(cycle iterations) at each
-        cycle iteration.
-    This implementation was adapted from the github repo: `bckenstler/CLR`_
-    Args:
-        optimizer (Optimizer): Wrapped optimizer.
-        base_lr (float or list): Initial learning rate which is the
-            lower boundary in the cycle for eachparam groups.
-            Default: 0.001
-        max_lr (float or list): Upper boundaries in the cycle for
-            each parameter group. Functionally,
-            it defines the cycle amplitude (max_lr - base_lr).
-            The lr at any cycle is the sum of base_lr
-            and some scaling of the amplitude; therefore
-            max_lr may not actually be reached depending on
-            scaling function. Default: 0.006
-        step_size (int): Number of training iterations per
-            half cycle. Authors suggest setting step_size
-            2-8 x training iterations in epoch. Default: 2000
-        mode (str): One of {triangular, triangular2, exp_range}.
-            Values correspond to policies detailed above.
-            If scale_fn is not None, this argument is ignored.
-            Default: 'triangular'
-        gamma (float): Constant in 'exp_range' scaling function:
-            gamma**(cycle iterations)
-            Default: 1.0
-        scale_fn (function): Custom scaling policy defined by a single
-            argument lambda function, where
-            0 <= scale_fn(x) <= 1 for all x >= 0.
-            mode paramater is ignored
-            Default: None
-        scale_mode (str): {'cycle', 'iterations'}.
-            Defines whether scale_fn is evaluated on
-            cycle number or cycle iterations (training
-            iterations since start of cycle).
-            Default: 'cycle'
-        last_batch_iteration (int): The index of the last batch. Default: -1
-    Example:
-        >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
-        >>> scheduler = torch.optim.CyclicLR(optimizer)
-        >>> data_loader = torch.utils.data.DataLoader(...)
-        >>> for epoch in range(10):
-        >>>     for batch in data_loader:
-        >>>         scheduler.batch_step()
-        >>>         train_batch(...)
-    .. _Cyclical Learning Rates for Training Neural Networks: https://arxiv.org/abs/1506.01186
-    .. _bckenstler/CLR: https://github.com/bckenstler/CLR
-    """
-    def __init__(self, optimizer, base_lr=1e-3, max_lr=6e-3,
-                 step_size=2000, mode='triangular', gamma=1.,
-                 scale_fn=None, scale_mode='cycle', last_batch_iteration=-1):
-
-        if not isinstance(optimizer, torch.optim.Optimizer):
-            raise TypeError('{} is not an Optimizer'.format(
-                type(optimizer).__name__))
-        self.optimizer = optimizer
-
-        if isinstance(base_lr, list) or isinstance(base_lr, tuple):
-            if len(base_lr) != len(optimizer.param_groups):
-                raise ValueError("expected {} base_lr, got {}".format(
-                    len(optimizer.param_groups), len(base_lr)))
-            self.base_lrs = list(base_lr)
-        else:
-            self.base_lrs = [base_lr] * len(optimizer.param_groups)
-
-        if isinstance(max_lr, list) or isinstance(max_lr, tuple):
-            if len(max_lr) != len(optimizer.param_groups):
-                raise ValueError("expected {} max_lr, got {}".format(
-                    len(optimizer.param_groups), len(max_lr)))
-            self.max_lrs = list(max_lr)
-        else:
-            self.max_lrs = [max_lr] * len(optimizer.param_groups)
-
-        self.step_size = step_size
-
-        if mode not in ['triangular', 'triangular2', 'exp_range'] \
-                and scale_fn is None:
-            raise ValueError('mode is invalid and scale_fn is None')
-
-        self.mode = mode
-        self.gamma = gamma
-
-        if scale_fn is None:
-            if self.mode == 'triangular':
-                self.scale_fn = self._triangular_scale_fn
-                self.scale_mode = 'cycle'
-            elif self.mode == 'triangular2':
-                self.scale_fn = self._triangular2_scale_fn
-                self.scale_mode = 'cycle'
-            elif self.mode == 'exp_range':
-                self.scale_fn = self._exp_range_scale_fn
-                self.scale_mode = 'iterations'
-        else:
-            self.scale_fn = scale_fn
-            self.scale_mode = scale_mode
-
-        self.batch_step(last_batch_iteration + 1)
-        self.last_batch_iteration = last_batch_iteration
-
-    def batch_step(self, batch_iteration=None):
-        if batch_iteration is None:
-            batch_iteration = self.last_batch_iteration + 1
-        self.last_batch_iteration = batch_iteration
-        for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
-            param_group['lr'] = lr
-
-    def _triangular_scale_fn(self, x):
-        return 1.
-
-    def _triangular2_scale_fn(self, x):
-        return 1 / (2. ** (x - 1))
-
-    def _exp_range_scale_fn(self, x):
-        return self.gamma**(x)
-
-    def get_lr(self):
-        step_size = float(self.step_size)
-        cycle = np.floor(1 + self.last_batch_iteration / (2 * step_size))
-        x = np.abs(self.last_batch_iteration / step_size - 2 * cycle + 1)
-
-        lrs = []
-        param_lrs = zip(self.optimizer.param_groups,
-                        self.base_lrs, self.max_lrs)
-        for param_group, base_lr, max_lr in param_lrs:
-            base_height = (max_lr - base_lr) * np.maximum(0, (1 - x))
-            if self.scale_mode == 'cycle':
-                lr = base_lr + base_height * self.scale_fn(cycle)
-            else:
-                lr = base_lr + base_height * \
-                    self.scale_fn(self.last_batch_iteration)
-            lrs.append(lr)
-        return lrs
