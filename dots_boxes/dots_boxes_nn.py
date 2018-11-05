d9328c3ffac65eb7bb101961073e0fc2df51c05e
damien <damlobster@gmail.com>
2018-11-05 14:04:45 +0100

    reduce transient GPU memory pike

--- dots_boxes/dots_boxes_nn.py
+++ dots_boxes/dots_boxes_nn.py
@@ -1,3 +1,6 @@
+import logging
+logger = logging.getLogger(__name__)
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -46,11 +49,12 @@ class SimpleNN(nn.Module):
     def save_parameters(self, generation):
         filename = self.params.nn.chkpts_filename
         fn = filename.format(generation)
-        print("Model saved to:", fn, flush=True)
+        logger.info("Model saved to: %s", fn)
         torch.save(self.state_dict(), fn)
 
     def load_parameters(self, generation, to_device=None):
         filename = self.params.nn.chkpts_filename
         fn = filename.format(generation)
-        print("Model loaded from:", fn, flush=True)
-        self.load_state_dict(torch.load(fn, map_location=to_device))
+        logger.info("Model loaded from: %s", fn)
+        self.load_state_dict(torch.load(fn, map_location='cpu'))
+        self.to(to_device)
