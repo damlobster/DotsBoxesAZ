598f11408bc04efedafd0b678afdb6682d93c4a6
damien <damlobster@gmail.com>
2018-11-09 00:37:59 +0100

    handle thinking time + override mcts params

--- players.py
+++ players.py
@@ -1,5 +1,5 @@
 import logging
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 import math 
 from functools import partial
 import asyncio
@@ -15,9 +15,10 @@ import configuration as cfg
 class AZPlayer(mp.Process):
     def __init__(self, params, time_limit, requ_queue: mp.Queue, resp_queue: mp.Queue):
         super(AZPlayer, self).__init__()
+        params.self_play.mcts.temperature = {0:0}
         self.params = params
         self.models = {}
-        self.time_limit = self.time_limit
+        self.time_limit = time_limit
         self.requ_queue = requ_queue
         self.resp_queue = resp_queue
 
@@ -36,30 +37,32 @@ class AZPlayer(mp.Process):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
 
-        nn_wrapper = NeuralNetWrapper(None, self.params)
+        params = self.params
+        nn_wrapper = NeuralNetWrapper(None, params)
 
-        nn = AsyncBatchedProxy(nn_wrapper, batch_size=params.nn_batch_size,
-                                    timeout=params.nn_batch_timeout,
-                                    batch_builder=params.nn_batch_builder)
+        nn = AsyncBatchedProxy(nn_wrapper, batch_size=params.self_play.nn_batch_size,
+                                    timeout=params.self_play.nn_batch_timeout,
+                                    batch_builder=params.self_play.nn_batch_builder)
         nn_task = asyncio.ensure_future(nn.run(), loop=loop)
 
-        mcts_cfg = self.params.self_play.mcts
+        mcts_cfg = params.self_play.mcts
         while True:
             req = self.requ_queue.get()
             if req is None:
                 # we received a poison pill
                 break
 
-            game_uuid, generation, game_state = req
+            game_uuid, game_state, generation, time_limit = req
             nn_wrapper.set_model(self._load_model(generation))
 
             node = create_root_uct_node(game_state)
-            fut = asyncio.ensure_future(UCT_search(node, None, nn, mcts_cfg.mcts_cpuct, 
-                                mcts_cfg.max_async_searches, (0.0, 0.0), self.time_limit))
+            fut = asyncio.ensure_future(UCT_search(node, int(1e12), nn, mcts_cfg.mcts_cpuct, 
+                                mcts_cfg.max_async_searches, (0.0, 0.0), time_limit))
             loop.run_until_complete(fut)
             
             policy = fut.result()
             if policy.sum() > 0:
+                policy *= game_state.get_valid_moves()
                 # we greedly return the best action
                 self.resp_queue.put((game_uuid, policy.argmax()))
             else:
