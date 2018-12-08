e360d220de228a1c4b2650a12bba2df0b9e53957
damien <damlobster@gmail.com>
2018-12-08 14:53:29 +0100

    use symmetries + add resnet55 params

--- configuration.py
+++ configuration.py
@@ -3,7 +3,7 @@ from functools import partial
 
 from utils.utils import get_cuda_devices_list, DotDict
 from dots_boxes.dots_boxes_game import BoxesState, nn_batch_builder
-from dots_boxes.dots_boxes_nn import SimpleNN
+from dots_boxes.dots_boxes_nn import SymmetriesGenerator, SimpleNN
 from nn import ResNetZero, GenerationLrScheduler
 
 import logging
@@ -79,9 +79,9 @@ resnet20 = DotDict({
         "init": partial(BoxesState.init_static_fields, ((3, 3),)),
     },
     "self_play": {
-        "num_games": 1000,
-        "n_workers": 12,
-        "games_per_workers": 10,
+        "num_games": 2000,
+        "n_workers": 10,
+        "games_per_workers": 100,
         "reuse_mcts_tree": True,
         "noise": (1.0, 0.25),  # alpha, coeff
         "nn_batch_size": 48,
@@ -91,8 +91,8 @@ resnet20 = DotDict({
         "mcts": {
             "mcts_num_read": 800,
             "mcts_cpuct": 4.0,
-            # from 8th move we greedly take move with most visit count
-            "temperature": {0: 1.0}, #, 15: 1e-50},
+            # from 6th move we greedly take move with most visit count
+            "temperature": {0: 1, 3: 0.5, 6: 0.2, 9:0.05}, #, 15: 1e-50},
             "max_async_searches": 64,
         }
     },
@@ -115,11 +115,12 @@ resnet20 = DotDict({
         "chkpts_filename": "data/_exp_/model_gen{}.pt",
         "train_params": {
             "pos_average": True,
-            "nb_epochs": 1,
+            "symmetries": SymmetriesGenerator(), #lambda boards, pi, z: [boards], [pi], [z]
+            "nb_epochs": 2,
             "train_split": 0.9,
             "train_batch_size": 2048,
             "val_batch_size": 2048,
-            "lr_scheduler": GenerationLrScheduler({0: 1e-3, 20:5e-4, 40:1e-4, 60:5e-5}),
+            "lr_scheduler": GenerationLrScheduler({0: 1e-3, 20:5e-4,}),
             "lr": None,
             "adam_params": {
                 "betas": (0.9, 0.999),
@@ -149,8 +150,89 @@ resnet20 = DotDict({
     }
 })
 
+resnet55 = DotDict({
+    "data_root": "data/_exp_",
+    "hdf_file": "data/_exp_/sp_data.hdf",
+    "tensorboard_log": "data/tboard/_exp_",
+    "game": {
+        "clazz": BoxesState,
+        "init": partial(BoxesState.init_static_fields, ((5, 5),)),
+    },
+    "self_play": {
+        "num_games": 2000,
+        "n_workers": 10,
+        "games_per_workers": 10,
+        "reuse_mcts_tree": True,
+        "noise": (1.0, 0.25),  # alpha, coeff
+        "nn_batch_size": 48,
+        "nn_batch_timeout": 0.05,
+        "nn_batch_builder": nn_batch_builder,
+        "pytorch_devices": ["cuda:1", "cuda:0"],  # get_cuda_devices_list(),
+        "mcts": {
+            "mcts_num_read": 800,
+            "mcts_cpuct": 4.0,
+            # from 6th move we greedly take move with most visit count
+            "temperature": {0: 1, 6: 0.2, 9:0.05}, #, 15: 1e-50},
+            "max_async_searches": 64,
+        }
+    },
+    "elo": {
+        "hdf_file": "data/_exp_/elo_data.hdf",
+        "n_games": 20,
+        "n_workers": 10,
+        "games_per_workers": 2,
+        "self_play_override":{
+            "reuse_mcts_tree": False,
+            "noise": (0.0, 0.0),
+            "mcts":{
+                "mcts_num_read":800
+            }
+        }
+    },
+    "nn": {
+        "model_class": ResNetZero,
+        "pytorch_device": "cuda:1",
+        "chkpts_filename": "data/_exp_/model_gen{}.pt",
+        "train_params": {
+            "pos_average": True,
+            "symmetries": SymmetriesGenerator(), #lambda boards, pi, z: [boards], [pi], [z]
+            "nb_epochs": 2,
+            "train_split": 0.9,
+            "train_batch_size": 2048,
+            "val_batch_size": 2048,
+            "lr_scheduler": GenerationLrScheduler({0: 1e-3, 20:5e-4,}),
+            "lr": None,
+            "adam_params": {
+                "betas": (0.9, 0.999),
+                "weight_decay": 1e-4,
+            },
+        },
+        "model_parameters": {
+            "resnet": {
+                "in_channels": 3,
+                "nb_channels": 128,
+                "kernel_size": 3,
+                "nb_blocks": 10
+            },
+            "policy_head": {
+                "in_channels": 128,
+                "inner_channels":4,
+                "fc_in": 36*4,
+                "nb_actions": 72,
+            },
+            "value_head": {
+                "in_channels": 128,
+                "inner_channels": 1,
+                "fc_in": 1*36,
+                "fc_inner": 18
+            },
+        }
+    }
+})
+
 # configuration to use
-params = resnet20
+params = resnet55
+params.game.init()
 
 DEFAULT_LOGGING = {
     'version': 1,
@@ -170,7 +252,7 @@ DEFAULT_LOGGING = {
     'loggers': {
         'mcts': {
             'handlers': ['default'],
-            'level': 'INFO',
+            'level': 'WARNING',
         },
         'nn': {
             'handlers': ['default'],
@@ -180,13 +262,17 @@ DEFAULT_LOGGING = {
             'handlers': ['default'],
             'level': 'WARNING',
         },
-        'self_play': {
+        'dots_boxes_game': {
             'handlers': ['default'],
             'level': 'WARNING',
         },
+        'self_play': {
+            'handlers': ['default'],
+            'level': 'INFO',
+        },
         'utils.proxies':{
             'handlers': ['default'],
-            'level': 'INFO'
+            'level': 'WARNING'
         }
     }
 }
